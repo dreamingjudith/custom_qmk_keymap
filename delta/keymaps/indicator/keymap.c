5a0995b079fac8e3ada1a53a43a14d9eb20e2aa1
Judith Jeong <calliope1226@hotmail.com>
2022-10-16 13:30:26 +0900

    Update keymap.c

--- delta/keymaps/indicator/keymap.c
+++ delta/keymaps/indicator/keymap.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 Álvaro "Gondolindrim" Volpato <alvaro.volpato@usp.br>
+Copyright 2022 Judith Jeong
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
